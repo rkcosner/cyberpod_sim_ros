331ac1a6f305697a844e063a6752421429c8fe22
Ryan Cosner <rkcosner@caltech.edu>
2020-07-16 15:03:57 -0700

    change filter rate

--- src/sensor_node.cpp
+++ src/sensor_node.cpp
@@ -8,6 +8,7 @@ ros::NodeHandle *nhParams_;
 ros::ServiceServer srv_ui_;
 ros::Subscriber sub_state_;
 ros::Publisher pub_sensor_;
+ros::Publisher pub_state_measured_; 
 
 double enc_v_variance_,psi_variance_,psi_dot_variance_,theta_dot_variance_;
 cyberpod_sim_ros::state state_current_;
@@ -58,6 +59,31 @@ void stateCallback(const cyberpod_sim_ros::state::ConstPtr msg)
 	pub_sensor_.publish(sensor_msg);
 }
 
+void stateCallbackNoEKF(const cyberpod_sim_ros::state::ConstPtr msg)
+{
+	// Receive True State (no noise)
+	state_current_ = *msg;
+
+	cyberpod_sim_ros::state state_msg = state_current_;
+
+	double thetaDot = state_current_.thetaDot;
+
+	// add in random noise
+	
+	state_msg.x += ( ((double)rand()/(double)RAND_MAX) *2-1)*enc_v_variance_;\
+	state_msg.v += ( ((double)rand()/(double)RAND_MAX) *2-1)*enc_v_variance_;
+	state_msg.psi += 0; //( ((double)rand()/(double)RAND_MAX) *2-1)*psi_variance_;
+	state_msg.psiDot += -0.1;//( ((double)rand()/(double)RAND_MAX) *2-1)*psi_dot_variance_;
+
+	state_msg.stateVec[0] = state_msg.x; 
+	state_msg.stateVec[3] = state_msg.v;
+	state_msg.stateVec[5] = state_msg.psi;
+	state_msg.stateVec[6] = state_msg.psiDot; 
+	
+	// publish measured state
+	pub_sensor_.publish(state_msg);
+}
+
 int main (int argc, char *argv[])
 {
 	// Init ros
@@ -68,13 +94,15 @@ int main (int argc, char *argv[])
 	nh_ = new ros::NodeHandle();
 
 	// Init pubs, subs and srvs
-	sub_state_ = nh_->subscribe<cyberpod_sim_ros::state>("state_true", 1,stateCallback);
-	pub_sensor_ = nh_->advertise<cyberpod_sim_ros::sensor>("sensor", 1);
+	sub_state_ = nh_->subscribe<cyberpod_sim_ros::state>("state_true", 1, stateCallbackNoEKF);
+	pub_sensor_ = nh_->advertise<cyberpod_sim_ros::state>("state_measured", 1);
+	//pub_sensor_ = nh_->advertise<cyberpod_sim_ros::sensor>("sensor", 1);
 
 	// Retreive params
-	nhParams_->param<double>("enc_v_variance",enc_v_variance_,0.001);
-	nhParams_->param<double>("psi_variance",psi_variance_,0.001);
-	nhParams_->param<double>("psi_dot_variance",psi_dot_variance_,0.001);
+	double variance = 0.0; //0.001;
+	nhParams_->param<double>("enc_v_variance",enc_v_variance_, variance);//0.001);
+	nhParams_->param<double>("psi_variance",psi_variance_, variance); //0.001);
+	nhParams_->param<double>("psi_dot_variance",psi_dot_variance_, variance); //0.001);
 	nhParams_->param<double>("theta_dot_variance",theta_dot_variance_,0.001);
 
 	// Display node info
