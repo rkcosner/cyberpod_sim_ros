82234f285a162e8ec7b3c8e20bca9696babaeb06
sarah <xsarah.dean@gmail.com>
2020-07-24 15:13:44 -0700

    updating max feasible error logic

--- notebooks/max_feasible_error.py
+++ notebooks/max_feasible_error.py
@@ -5,11 +5,12 @@ from dynamics import dynamics
 
 # code from ECOS filter and ecos.launch
 
-L_Lgh = 1# 0.5
-L_Lfh = 1 #0.1
-L_ah = 5
+L_Lgh = 0.24338437673585678
+L_Lfh = 15.99427512538
+L_ah = 100
 alpha = 10
-C = 1 # TODO: not sure which param this should be
+alpha_e = 10
+C = 0.4
 
 # specifying upper_bd, lower_bd, num_grid
 default_grid_limits = np.array([[-1, 1, 20], # x
@@ -24,22 +25,37 @@ def get_fns(x):
     
     f,g = dynamics(x)
     x5_eq = 0.138
+
+    # previously,
     # todo: should only certain components count?
-    Lfh = -x[6]*(x[5] - x5_eq + f[6])
-    Lgh = -x[6]*g[6,[0,1]]  
-    ah = alpha * (-1/2*((x[5]-x5_eq)**2 + x[6]**2) + C)
+    # Lfh = -x[6]*(x[5] - x5_eq + f[6])
+    # Lgh = -x[6]*g[6,[0,1]]  
+    # ah = alpha * (-1/2*((x[5]-x5_eq)**2 + x[6]**2) + C)
     
-    return Lgh, Lfh, ah
+    Lfh_above = -f[6] - alpha_e*x[6] 
+    Lgh_above = -g[6] 
+    ah_above = alpha*(-x[6] + alpha_e*(C + x5_eq - x[5]))
+
+    Lfh_below = f[6] + alpha_e*x[6] 
+    Lgh_below = g[6] 
+    ah_below = alpha*(x[6] + alpha_e*(C - x5_eq + x[5]))
+
+    return (Lgh_above, Lfh_above, ah_above), (Lgh_below, Lfh_below, ah_below)
 
 def get_bd(x):
-    Lgh, Lfh, ah = get_fns(x)
-    return max(np.linalg.norm(Lgh) / (2*L_Lgh), (Lfh + ah)/(2*(L_Lfh+L_ah)))
+    # Lgh, Lfh, ah = get_fns(x)
+    fns_above, fns_below = get_fns(x)
+    Lgh_above, Lfh_above, ah_above = fns_above
+    Lgh_below, Lfh_below, ah_below = fns_below
+    bd_above = max(np.linalg.norm(Lgh_above) / (2*L_Lgh), (Lfh_above + ah_above)/(2*(L_Lfh+L_ah)))
+    bd_below = max(np.linalg.norm(Lgh_below) / (2*L_Lgh), (Lfh_below + ah_below)/(2*(L_Lfh+L_ah)))
+    return min(bd_above, bd_below)
 
 def get_gridded_eps(grid_limits=default_grid_limits, selected_inds=[0,5]):
 	assert grid_limits.shape[0] == 7
 
 	# computing bound on eps for all of these values
-	eps_of_x = -np.inf * np.ones(grid_limits[:,2])
+	eps_of_x = -np.inf * np.ones(grid_limits[:,2].astype(int))
 	for i0, x0 in enumerate(np.linspace(*grid_limits[0])):
 	    for i1, x1 in enumerate(np.linspace(*grid_limits[1])):
 	        for i2, x2 in enumerate(np.linspace(*grid_limits[2])):
