c01f60f1c25934c4f01c13fa330bba9ef34a3ff4
Ryan Cosner <rkcosner@caltech.edu>
2020-12-30 19:02:35 -0800

    adjust constants

--- src/sensor_node.cpp
+++ src/sensor_node.cpp
@@ -10,7 +10,7 @@ ros::Subscriber sub_state_;
 ros::Publisher pub_sensor_;
 ros::Publisher pub_state_measured_; 
 
-double enc_v_variance_,psi_error_,psi_dot_error_,theta_dot_variance_;
+double enc_v_variance_, theta_error_, theta_dot_error_, theta_dot_variance_;
 cyberpod_sim_ros::state state_current_;
 double dt_,v_last_,theta_last_;
 double encL_pos_,encR_pos_;
@@ -18,48 +18,6 @@ double Rw_ = 0.195;
 double L_ = 0.5;
 
 void stateCallback(const cyberpod_sim_ros::state::ConstPtr msg)
-{
-	//compute sensor values (no noise)
-	state_current_ = *msg;
-	double encoderL_vel = (state_current_.v - state_current_.thetaDot*L_)/Rw_;
-	double encoderR_vel = (state_current_.v + state_current_.thetaDot*L_)/Rw_;
-
-	double thetaDot = state_current_.thetaDot;
-
-	double psi = state_current_.psi;
-	double psiDot = state_current_.psiDot;
-
-	double steering = 0.;
-	double battV = 60.;
-
-	// add in random noise
-	encoderL_vel += ( ((double)rand()/(double)RAND_MAX) *2-1)*enc_v_variance_;
-	encoderR_vel += ( ((double)rand()/(double)RAND_MAX) *2-1)*enc_v_variance_;
-	psi += psi_error_;
-	psiDot += psi_dot_error_;
-	thetaDot += ( ((double)rand()/(double)RAND_MAX) *2-1)*theta_dot_variance_;
-
-	//update encoder positions;
-	encL_pos_ += encoderL_vel*dt_;
-	encR_pos_ += encoderR_vel*dt_;
-
-	cyberpod_sim_ros::sensor sensor_msg;
-
-	sensor_msg.time = (uint32_t) (state_current_.time*1E6);
-	sensor_msg.data.push_back(encL_pos_);
-	sensor_msg.data.push_back(encR_pos_);
-	sensor_msg.data.push_back(encoderL_vel);
-	sensor_msg.data.push_back(encoderR_vel);
-	sensor_msg.data.push_back(thetaDot);
-	sensor_msg.data.push_back(psi);
-	sensor_msg.data.push_back(psiDot);
-	sensor_msg.data.push_back(steering);
-	sensor_msg.data.push_back(battV);
-
-	pub_sensor_.publish(sensor_msg);
-}
-
-void stateCallbackNoEKF(const cyberpod_sim_ros::state::ConstPtr msg)
 {
 	// Receive True State (no noise)
 	state_current_ = *msg;
@@ -72,8 +30,8 @@ void stateCallbackNoEKF(const cyberpod_sim_ros::state::ConstPtr msg)
 	
 	state_msg.x += ( ((double)rand()/(double)RAND_MAX) *2-1)*enc_v_variance_;
 	state_msg.v += ( ((double)rand()/(double)RAND_MAX) *2-1)*enc_v_variance_;
-	state_msg.psi += psi_error_; //( ((double)rand()/(double)RAND_MAX) *2-1)*psi_variance_;
-	state_msg.psiDot += psi_dot_error_;//( ((double)rand()/(double)RAND_MAX) *2-1)*psi_dot_variance_;
+	state_msg.psi += theta_error_; //( ((double)rand()/(double)RAND_MAX) *2-1)*psi_variance_;
+	state_msg.psiDot += theta_dot_error_;//( ((double)rand()/(double)RAND_MAX) *2-1)*psi_dot_variance_;
 
 	state_msg.stateVec[0] = state_msg.x; 
 	state_msg.stateVec[3] = state_msg.v;
@@ -94,20 +52,21 @@ int main (int argc, char *argv[])
 	nh_ = new ros::NodeHandle();
 
 	// Init pubs, subs and srvs
-	sub_state_ = nh_->subscribe<cyberpod_sim_ros::state>("state_true", 1, stateCallbackNoEKF);
+	sub_state_ = nh_->subscribe<cyberpod_sim_ros::state>("state_true", 1, stateCallback);
 	pub_sensor_ = nh_->advertise<cyberpod_sim_ros::state>("state_measured", 1);
 	//pub_sensor_ = nh_->advertise<cyberpod_sim_ros::sensor>("sensor", 1);
 
 	// Retreive params
 	double variance = 0.0; //0.001;
 	nhParams_->param<double>("enc_v_variance",enc_v_variance_, variance);//0.001);
-	nhParams_->param<double>("psi_error",psi_error_, 0.0); //0.001);
-	nhParams_->param<double>("psi_dot_error",psi_dot_error_, 0.0); //0.001);
+	nhParams_->param<double>("theta_error",theta_error_, 0.0); //0.001);
+	nhParams_->param<double>("theta_dot_error",theta_dot_error_, 0.0); //0.001);
 	nhParams_->param<double>("theta_dot_variance",theta_dot_variance_,0.001);
 
 	// Display node info
-	ROS_INFO("Sensor node successfuly started!");
-
+	ROS_INFO("Sensor node successfuly started with:");
+	ROS_INFO("___theta_error=%4f", theta_error_);
+	ROS_INFO("___theta_dot_error=%4f", theta_dot_error_);
 	ros::spin();
 
 	return 0;
